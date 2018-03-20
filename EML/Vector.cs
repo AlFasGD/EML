9f55d3700a1195c60ff59d2eada78f8fd5fd76ea
Alex <kalfas.alex@gmail.com>
2018-03-20 23:27:08 +0200

    Enhanced vectors

---
+++ EML/Vector.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EML
+{
+    /// <summary>Represents a multi-dimensional vector.</summary>
+    public struct Vector
+    {
+        /// <summary>The values of the vector.</summary>
+        public RealNumberExpression[] Values;
+        /// <summary>Gets the number of dimensions of the vector.</summary>
+        public int Dimensions { get => Values.Length; }
+
+        /// <summary>Creates a new instance of <seealso cref="Vector"/>.</summary>
+        /// <param name="v">The values of the vector.</param>
+        public Vector(params RealNumberExpression[] v)
+        {
+            Values = v;
+        }
+        /// <summary>Creates a new instance of <seealso cref="Vector"/>.</summary>
+        /// <param name="start">The coordinates of the starting point.</param>
+        /// <param name="end">The coordinates of the ending point.</param>
+        public Vector(RealNumberExpression[] start, RealNumberExpression[] end)
+        {
+            if (start.Length == end.Length)
+            {
+                RealNumberExpression[] v = new RealNumberExpression[start.Length];
+                for (int i = 0; i < v.Length; i++)
+                    v[i] = end[i] - start[i];
+                Values = v;
+            }
+            else throw new ArgumentException();
+        }
+        
+        public static Vector operator +(Vector left, Vector right)
+        {
+            if (left.Dimensions == right.Dimensions)
+            {
+                Vector v = new Vector(new RealNumberExpression[left.Dimensions]);
+                for (int i = 0; i < v.Dimensions; i++)
+                    v[i] = left[i] + right[i];
+                return v;
+            }
+            else throw new ArgumentException();
+        }
+        public static Vector operator -(Vector left, Vector right)
+        {
+            if (left.Dimensions == right.Dimensions)
+            {
+                Vector v = new Vector(new RealNumberExpression[left.Dimensions]);
+                for (int i = 0; i < v.Dimensions; i++)
+                    v[i] = left[i] - right[i];
+                return v;
+            }
+            else throw new ArgumentException();
+        }
+
+        /// <summary>Gets the value of the vector at the specified dimension.</summary>
+        /// <param name="i">The dimension to get the value of the vector.</param>
+        public RealNumberExpression this[int i]
+        {
+            get => Values[i];
+            set => Values[i] = value;
+        }
+
+        /// <summary>Converts the vector to its string representation.</summary>
+        public override string ToString()
+        {
+            StringBuilder result = new StringBuilder("(");
+            for (int i = 0; i < Values.Length; i++)
+                result.Append(Values[i].ToString() + ", ");
+            result = result.Remove(result.Length - 2, 2);
+            result.Append(")");
+            return result.ToString();
+        }
+    }
+}
--- EML/Vector2D.cs
+++ EML/Vector2D.cs
@@ -32,13 +32,33 @@ namespace EML
             X = endingX - startingX;
             Y = endingY - startingY;
         }
+        /// <summary>Creates a new instance of <seealso cref="Vector2D"/>.</summary>
+        /// <param name="v">The value tuple containing the coordinations of the vector.</param>
+        public Vector2D((RealNumberExpression, RealNumberExpression) v)
+        {
+            X = v.Item1;
+            Y = v.Item2;
+        }
+        /// <summary>Creates a new instance of <seealso cref="Vector2D"/>.</summary>
+        /// <param name="start">The value tuple containing the starting coordinations of the vector.</param>
+        /// <param name="end">The value tuple containing the ending coordinations of the vector.</param>
+        public Vector2D((RealNumberExpression, RealNumberExpression) start, (RealNumberExpression, RealNumberExpression) end)
+        {
+            X = end.Item1 - start.Item1;
+            Y = end.Item2 - start.Item2;
+        }
 
         public static Vector2D operator +(Vector2D left, Vector2D right) => new Vector2D(left.X + right.X, left.Y + right.Y);
         public static Vector2D operator -(Vector2D left, Vector2D right) => new Vector2D(left.X - right.X, left.Y - right.Y);
 
+        /// <summary>Returns the inner product of the vector.</summary>
+        /// <param name="v">The vector to get the inner product of.</param>
         public static RealNumberExpression InnerProduct(Vector2D v)
         {
             return new RealNumberExpression("iv", new LargeDecimal[0], new OperationType[0]);
         }
+
+        /// <summary>Converts the vector to its string representation.</summary>
+        public override string ToString() => "(" + X.ToString() + ", " + Y.ToString() + ")";
     }
 }
