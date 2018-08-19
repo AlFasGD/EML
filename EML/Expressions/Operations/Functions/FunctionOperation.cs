8d8ad946c8a43d5d504a88a8761d644aa0f025c2
AlFas <AlFasGD@users.noreply.github.com>
2018-08-19 12:08:37 +0300

    Add absolute value operation

--- EML/Expressions/Operations/Functions/FunctionOperation.cs
+++ EML/Expressions/Operations/Functions/FunctionOperation.cs
@@ -6,8 +6,10 @@ using System.Threading.Tasks;
 
 namespace EML.Expressions.Operations.Functions
 {
+    /// <summary>Represents a predefined mathematical function.</summary>
     public abstract class FunctionOperation : Operation
     {
+        /// <summary>The argument of the function.</summary>
         public Expression Argument;
     }
 }
---
+++ EML/Expressions/Operations/Functions/General/Absolute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EML.Expressions.Operations.Functions.General
+{
+    /// <summary>Represents the absolute value operation.</summary>
+    public class Absolute : FunctionOperation
+    {
+        public Absolute(Expression argument)
+        {
+            Argument = argument;
+        }
+    }
+}
