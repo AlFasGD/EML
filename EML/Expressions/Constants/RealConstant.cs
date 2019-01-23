8094969f84d1b5a65748962b0c6f44a5a4e26262
AlFas <AlFasGD@users.noreply.github.com>
2019-01-23 21:06:29 +0200

    Add IConstant

--- EML/Expressions/Constants/RealConstant.cs
+++ EML/Expressions/Constants/RealConstant.cs
@@ -8,20 +8,36 @@ using System.Threading.Tasks;
 namespace EML.Expressions.Constants
 {
     /// <summary>Represents a real number constant.</summary>
-    public sealed class RealConstant : RealNumericExpression
+    public sealed class RealConstant : RealNumericExpression, IConstant
     {
+        /// <summary>The name of the constant.</summary>
+        public string Name { get; set; }
         /// <summary>The value of the real number constant.</summary>
         public LargeDecimal Value { get; }
 
-        /// <summary>Initializes a new instance of the <seealso cref="RealConstant"/> class.<</summary>
+        /// <summary>Initializes a new instance of the <seealso cref="RealConstant"/> class.</summary>
         /// <param name="value">The value of the <seealso cref="RealConstant"/>.</param>
         public RealConstant(LargeDecimal value) : this(null, value) { }
-        /// <summary>Initializes a new instance of the <seealso cref="RealConstant"/> class.<</summary>
+        /// <summary>Initializes a new instance of the <seealso cref="RealConstant"/> class.</summary>
         /// <param name="value">The value of the <seealso cref="RealConstant"/>.</param>
         public RealConstant(string name, LargeDecimal value)
-            : base(name)
         {
+            Name = name;
             Value = value;
         }
+
+        public static bool operator <(RealConstant left, RealConstant right) => left.Value < right.Value;
+        public static bool operator >(RealConstant left, RealConstant right) => left.Value > right.Value;
+        public static bool operator >=(RealConstant left, RealConstant right) => left.Value >= right.Value;
+        public static bool operator <=(RealConstant left, RealConstant right) => left.Value <= right.Value;
+        public static bool operator ==(RealConstant left, RealConstant right) => left.Value == right.Value;
+        public static bool operator !=(RealConstant left, RealConstant right) => left.Value != right.Value;
+
+        protected override bool LessThan(RealNumericExpression expression) => Value < (expression as RealConstant).Value;
+        protected override bool GreaterThan(RealNumericExpression expression) => Value > (expression as RealConstant).Value;
+        protected override bool LessThanOrEqualTo(RealNumericExpression expression) => Value <= (expression as RealConstant).Value;
+        protected override bool GreaterThanOrEqualTo(RealNumericExpression expression) => Value >= (expression as RealConstant).Value;
+        protected override bool EqualTo(RealNumericExpression expression) => Value == (expression as RealConstant).Value;
+        protected override bool DifferentThan(RealNumericExpression expression) => Value != (expression as RealConstant).Value;
     }
 }
---
+++ EML/Expressions/IConstant.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EML.Expressions
+{
+    /// <summary>Represents a constant.</summary>
+    public interface IConstant
+    {
+        /// <summary>The name of the constant.</summary>
+        string Name { get; }
+    }
+}
