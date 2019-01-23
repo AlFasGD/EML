5e7a26178772240d84d78029682fc5f31f635fa3
AlFas <AlFasGD@users.noreply.github.com>
2019-01-23 21:09:25 +0200

    Implement IVariable

---
+++ EML/Expressions/IVariable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EML.Expressions
+{
+    /// <summary>Represents a variable.</summary>
+    public interface IVariable
+    {
+        /// <summary>The name of the variable.</summary>
+        string Name { get; }
+    }
+}
